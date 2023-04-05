4f7a0d838c6860c2bf3b68808859e47445da9e26
ahadtaghiyev <106157931+AhadDV@users.noreply.github.com>
2023-04-05 17:50:41 +0400

    Add files via upload

---
+++ P234 05.04.2023/P234 05.04.2023/Program.cs
@@ -0,0 +1,126 @@
+﻿
+
+//////int number = 5;
+
+//////int number2 = 10;
+
+//////int number3 = number;
+
+//////number3 = 10;
+
+//////Console.WriteLine(number);
+//////Console.WriteLine(number3);
+
+//////int[] Ints = { 1, 5, 3, 4 };
+
+//////Console.WriteLine(Ints.Length);
+
+//////int[] Ints2 = Ints;
+
+//////int[] Ints3 = Ints2;
+
+//////Console.WriteLine(Ints==Ints3);
+
+
+//////string name = "Xasay";
+
+//////string name2 = name;
+
+//////Console.WriteLine(name==name2);
+
+//////string name3 = "Xasay";
+
+//////using System.Text;
+
+//////int number = 10;
+
+
+
+//////string name = "Izzat";
+
+////////char a = 'Z';
+
+//////for(int i = 0;i < name.Length; i++)
+//////{
+//////    Console.WriteLine(name[i]);
+//////}
+
+
+
+////int num=10;   //--123
+//////Console.WriteLine(num);
+////Change(ref num);
+//////Console.WriteLine(num);
+
+
+
+////void Change(ref int num1 )
+////{
+
+////    num1 = 0;
+////    //num1 = 100;
+////}
+
+
+
+
+////string name = "ahAd";
+
+
+////name = char.ToUpper(name[0])+name.Substring(1,3).ToLower();
+
+////name=name.ToLower();
+
+////'a' "A"
+////string FirstLetter = char.ToUpper(name[0]).ToString();
+
+
+////string OtherLetters = name.Substring(1, name.Length - 1);
+
+////string result=FirstLetter+ OtherLetters.ToLower();
+
+////Console.WriteLine(result);
+
+////name = char.ToUpper(name[0]) + name.Substring(1,name.Length-1).ToLower();
+
+////Console.WriteLine(name);
+
+////string na
+
+
+////string data = "";
+
+////string name = "Azerbaijan";
+////string data = string.Empty;
+
+////        //"a"
+
+
+//////data = data + 'a';
+
+
+////for(int i = 0;i < name.Length; i++)
+////{
+////    if (name[i] == 'a')
+////    {
+////        //data = data + name[i];
+////        data += name[i];
+////    }
+////}
+////Console.WriteLine(data);
+
+
+//string name = "MahammadMustafa";
+//                //EMrah
+// string result= CustomSubstring(name, 10, 30);
+
+//Console.WriteLine(result);
+
+
+
+//string CustomSubstring(string data, int startindex, int endindex)
+//{
+
+//    string name = "Amrah";
+//    return name;
+//}
